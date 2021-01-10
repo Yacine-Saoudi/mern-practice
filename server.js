2b80b585fd59a7cdac0fa3b151311116b4c13da7
Yacine-Saoudi <yac.sao453@gmail.com>
2021-01-10 05:09:25 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send("API RUNNING"));
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => {
+    console.log('SERBER STARTO')
+});
